817ebcd958067daaac08dac873f70a2de3065af6
Prem Maity <premmaity2003@gmail.com>
2023-10-02 22:40:48 +0530

    first commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../styles/navbar.css'
+
+
+export const Navbar = ({size,setShow}) => {
+  return (
+<nav>
+    <div className='nav_box'>
+      <span className='my_shop'  onClick={()=>setShow(true)}>
+        My Shopping
+      </span>
+    <div className='cart' onClick={()=>setShow(false)}>
+        <span><i className="fas fa-cart-plus"></i></span>
+        <span>{size}</span>
+    </div>
+    </div>
+</nav>
+  )
+}
